e1de164ffaff19172a333bae6796a707c922b87e
ashirchkov <sa@task1c.ru>
2025-06-26 17:17:30 +0400

    Поправил обнаружение мониторингом воркеров

--- lib/monitoring/consumercounter.php
+++ lib/monitoring/consumercounter.php
@@ -14,10 +14,10 @@ class ConsumerCounter
      *
      * @return int
      */
-    public function get(string $command = 'messenger:consume'): int
+    public function get(string $command = 'messenger:consume|bsi\.queue:consume'): int
     {
-        $output = shell_exec(sprintf('ps aux | grep -v grep | grep \'%s\'', escapeshellarg($command)));
+        $output = shell_exec(sprintf('ps aux | grep -v grep | grep -i -E %s', escapeshellarg($command)));
 
-        return substr_count($output ?: '', $command);
+        return $output ? count(explode(PHP_EOL, trim($output))) : 0;
     }
 }
