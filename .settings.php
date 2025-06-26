5e57a87368dd09712ad0ed1cd9bc26904cb701d6
ashirchkov <sa@task1c.ru>
2025-06-26 16:27:16 +0400

    Поддержка Bitrix CLI

--- .settings.php
+++ .settings.php
@@ -1,10 +1,20 @@
 <?php
 
+use Bsi\Queue\Console\ConsoleCommand;
+
 return [
     'controllers' => [
         'value' => [
             'namespaces' => [
                 'Bsi\\Queue\\Monitoring\\Controller' => 'api',
+            ],
+        ],
+        'readonly' => true,
+    ],
+    'console' => [
+        'value' => [
+            'commands' => [
+                ConsoleCommand::class,
             ],
         ],
         'readonly' => true,
