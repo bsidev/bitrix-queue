95b061ba7784d4c537a4d7a48f256719b018ceeb
ashirchkov <sa@task1c.ru>
2025-06-27 16:18:53 +0400

    Падал messenger:stop-workers/bsi.queue:stop-workers. Исправил некорректную работу кеша.

--- lib/cache/bitrixcacheinterface.php
+++ lib/cache/bitrixcacheinterface.php
@@ -7,8 +7,8 @@ interface BitrixCacheInterface
     public function forceRewriting(bool $value): void;
     public function cleanDir(?string $initDir = null, ?string $baseDir = null): void;
     public function initCache(int $ttl, string $uniqueString, ?string $initDir = null, ?string $baseDir = null): bool;
-    public function getVars(): array;
-    public function startDataCache(?int $ttl = null, ?string $uniqueString = null, ?string $initDir = null, array $vars = [], ?string $baseDir = null): bool;
-    public function endDataCache(?array $vars = null): void;
+    public function getVars(): mixed;
+    public function startDataCache(?int $ttl = null, ?string $uniqueString = null, ?string $initDir = null, mixed $vars = null, ?string $baseDir = null): bool;
+    public function endDataCache(mixed $vars = null): void;
     public function clean(string $uniqueString, ?string $initDir = null, ?string $baseDir = null): void;
 }
