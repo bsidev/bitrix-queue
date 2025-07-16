e1acb790e7c5540acba9ce9dc15bb51f2e646f23
Sergey Balasov <sbalasov@gmail.com>
2025-07-16 09:26:56 +0700

    Добавлена поддержка модуля в папке local (Closes #83)

--- install/admin/bsi_queue_dashboard.php
+++ install/admin/bsi_queue_dashboard.php
@@ -1,3 +1,7 @@
 <?php
 
-require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/bsi.queue/admin/dashboard.php');
+if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/bsi.queue/admin/dashboard.php')) {
+    require($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/bsi.queue/admin/dashboard.php');
+} else {
+    require($_SERVER['DOCUMENT_ROOT'] . '/local/modules/bsi.queue/admin/dashboard.php');
+}
